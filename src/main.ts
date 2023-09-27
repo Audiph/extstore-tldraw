896e54e9d97edeacd59d3b58bdb50bdd3418bb46
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-09-27 21:10:17 +0800

    testing runtime modules

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+console.log('Hello World!')
+
+ext.runtime.onEnable.addListener(() => {
+  console.log('Extension Enabled')
+})
+
+ext.runtime.onExtensionClick.addListener(() => {
+  console.log('Extension Clicked')
+})
