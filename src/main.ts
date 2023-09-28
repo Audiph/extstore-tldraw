03a260cd1593e00a441f93c0aac33f061053dfdc
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-09-28 19:01:15 +0800

    format code with prettier

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
-import { getItem } from "./utils"
+import { getItem } from './utils';
 
-let tabs: ext.tabs.Tab[] = []
-let windows: ext.windows.Window[] = []
-let webviews: ext.webviews.Webview[] = []
-let websessions: ext.websessions.Websession[] = []
-
+let tabs: ext.tabs.Tab[] = [];
+let windows: ext.windows.Window[] = [];
+let webviews: ext.webviews.Webview[] = [];
+let websessions: ext.websessions.Websession[] = [];
 
 // ext.runtime.onEnable.addListener(() => {
 //   console.log('enabled');
-//   tabs = [] 
-//   windows = [] 
+//   tabs = []
+//   windows = []
 //   webview = null
 // })
 
@@ -20,72 +19,77 @@ ext.runtime.onExtensionClick.addListener(async () => {
     icon_dark: 'icons/icon-128-dark.png',
     muted: true,
     mutable: false,
-    closable: true
-  })
+    closable: true,
+  });
 
   const newWindow = await ext.windows.create({
     title: newTab.text,
     icon: 'icons/icon-1024.png',
     fullscreenable: true,
     vibrancy: false,
-    darkMode: 'platform'
-  })
+    darkMode: 'platform',
+  });
+
+  const newWindowSize = await ext.windows.getContentSize(newWindow.id);
 
   const newWebsession = await ext.websessions.create({
     partition: newWindow.id,
     persistent: true,
     global: false,
-    cache: true
-  })
-
-  const newWindowSize = await ext.windows.getContentSize(newWindow.id)
+    cache: true,
+  });
 
   const newWebview = await ext.webviews.create({
     window: newWindow,
     websession: newWebsession,
-    bounds: { x: 0, y: 0, width: newWindowSize.width, height: newWindowSize.height },
-    autoResize: { width: true, height: true }
-  })
-  tabs.push(newTab)
-  windows.push(newWindow)
-  websessions.push(newWebsession)
-  webviews.push(newWebview)
+    bounds: {
+      x: 0,
+      y: 0,
+      width: newWindowSize.width,
+      height: newWindowSize.height,
+    },
+    autoResize: { width: true, height: true },
+  });
+  tabs.push(newTab);
+  windows.push(newWindow);
+  websessions.push(newWebsession);
+  webviews.push(newWebview);
 
-  await ext.webviews.loadFile(newWebview.id, 'index.html')
-})
+  await ext.webviews.loadFile(newWebview.id, 'index.html');
+});
 
 ext.tabs.onClickedClose.addListener(async (event) => {
-  const getTab = getItem(tabs, event) as ext.tabs.Tab
-  const getWindow = getItem(windows, event) as ext.windows.Window
+  const getTab = getItem(tabs, event) as ext.tabs.Tab;
+  const getWindow = getItem(windows, event) as ext.windows.Window;
   // safety check, make sure tab exists
   if (getTab && getTab.id) {
     // remove (delete) the tab when the close button is clicked
-    tabs = tabs.filter(tab => tab.id !== event.id)
-    await ext.tabs.remove(getTab.id)
+    tabs = tabs.filter((tab) => tab.id !== event.id);
+    await ext.tabs.remove(getTab.id);
   }
 
   // safety check, make sure window exists
   if (getWindow && getWindow.id) {
     // remove (delete) the window
-    windows = windows.filter(window => window.id !== event.id)
-    await ext.windows.remove(getWindow.id)
+    windows = windows.filter((window) => window.id !== event.id);
+    await ext.windows.remove(getWindow.id);
   }
-})
+});
 
 ext.tabs.onClicked.addListener(async (event) => {
-  const getWindow = getItem(windows, event) as ext.windows.Window
+  const getWindow = getItem(windows, event) as ext.windows.Window;
   if (getWindow && getWindow.id) {
-    await ext.windows.restore(getWindow.id)
-    await ext.windows.focus(getWindow.id)
+    await ext.windows.restore(getWindow.id);
+    await ext.windows.focus(getWindow.id);
   }
-})
+});
 
 // listen for window close events
 ext.windows.onClosed.addListener(async (event) => {
-  const getTab = getItem(tabs, event) as ext.tabs.Tab
+  const getTab = getItem(tabs, event) as ext.tabs.Tab;
   if (getTab && getTab.id) {
-    tabs = tabs.filter(tab => tab.id !== event.id)
-    windows = windows.filter(window => window.id !== event.id)
-    await ext.tabs.remove(getTab.id)
+    tabs = tabs.filter((tab) => tab.id !== event.id);
+    windows = windows.filter((window) => window.id !== event.id);
+    await ext.tabs.remove(getTab.id);
   }
-})
+});
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,6 @@
-export const getItem = (items: ext.tabs.Tab[] | ext.windows.Window[], target: ext.tabs.TabEvent | ext.windows.WindowEvent) => {
-  return items.find(item => item.id === target.id)
-}
+export const getItem = (
+  items: ext.tabs.Tab[] | ext.windows.Window[],
+  target: ext.tabs.TabEvent | ext.windows.WindowEvent
+) => {
+  return items.find((item) => item.id === target.id);
+};
