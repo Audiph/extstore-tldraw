fbcdd3470e3aa3a14bae3a1930c4bd7a77179609
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-09-29 07:38:56 +0800

    Refactor to use constants files and move JS executes to constants
    Refactoring constants for more readable code

--- src/common/types.ts
+++
@@ -1,2 +0,0 @@
-export type ExtApiTypes = ext.tabs.Tab | ext.windows.Window;
-export type ExtEventTypes = ext.tabs.TabEvent | ext.windows.WindowEvent;
--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,4 +1,10 @@
-import { ExtApiTypes, ExtEventTypes } from './types';
+import {
+  ExtApiTypes,
+  ExtBranding,
+  ExtEventTypes,
+  TLDrawDarkTheme,
+  TLDrawLightTheme,
+} from './constants';
 
 /** Gets item needed to be used
  *  @param items Array of Objects to choose from
@@ -34,34 +40,18 @@ export const changeTLDrawTheme = async (
   if (isDarkMode) {
     // setup icon and theme to dark if isDarkMode is true
     await ext.windows.setIcon(window.id, 'icons/icon-128-dark.png');
-    await ext.webviews.executeJavaScript(
-      webview.id,
-      `document.querySelector('.tl-container').className = 'tl-container tl-theme__dark'`
-    );
+    await ext.webviews.executeJavaScript(webview.id, TLDrawDarkTheme);
   } else {
     // setup icon and theme to light if isDarkMode is false
     await ext.windows.setIcon(window.id, 'icons/icon-1024.png');
-    await ext.webviews.executeJavaScript(
-      webview.id,
-      `document.querySelector('.tl-container').className = 'tl-container tl-theme__light'`
-    );
+    await ext.webviews.executeJavaScript(webview.id, TLDrawLightTheme);
   }
   /**
    * Since the initial open of window is not working, setting it up here will work.
    * Whenever onUpdatedDarkMode runs, button menus will update and EXT-branding button will be added
    * @param extButton button id that's been setup so we can add an event listener to redirect to ext.store whenever clicked
    */
-  await ext.webviews.executeJavaScript(
-    webview.id,
-    `
-      document.querySelector('.tlui-toolbar__tools').innerHTML = '<button draggable="false" type="button" class="tlui-button tlui-button__normal tlui-toolbar__tools__button" data-testid="tools.select" data-tool="select" data-geo="" aria-label="tool.select" title="Select — V" data-state="selected"><div class="tlui-icon" style="-webkit-mask: url(&quot;https://unpkg.com/@tldraw/assets@2.0.0-alpha.14/icons/icon/tool-pointer.svg&quot;) center 100% / 100% no-repeat;"></div></button><button draggable="false" type="button" class="tlui-button tlui-button__normal tlui-toolbar__tools__button" data-testid="tools.hand" data-tool="hand" data-geo="" aria-label="tool.hand" title="Hand — H"><div class="tlui-icon" style="-webkit-mask: url(&quot;https://unpkg.com/@tldraw/assets@2.0.0-alpha.14/icons/icon/tool-hand.svg&quot;) center 100% / 100% no-repeat;"></div></button><div class="tlui-toolbar__divider"></div><button draggable="false" type="button" class="tlui-button tlui-button__normal tlui-toolbar__tools__button" data-testid="tools.draw" data-tool="draw" data-geo="" aria-label="tool.draw" title="Draw — D"><div class="tlui-icon" style="-webkit-mask: url(&quot;https://unpkg.com/@tldraw/assets@2.0.0-alpha.14/icons/icon/tool-pencil.svg&quot;) center 100% / 100% no-repeat;"></div></button><button draggable="false" type="button" class="tlui-button tlui-button__normal tlui-toolbar__tools__button" data-testid="tools.eraser" data-tool="eraser" data-geo="" aria-label="tool.eraser" title="Eraser — E"><div class="tlui-icon" style="-webkit-mask: url(&quot;https://unpkg.com/@tldraw/assets@2.0.0-alpha.14/icons/icon/tool-eraser.svg&quot;) center 100% / 100% no-repeat;"></div></button><div class="tlui-toolbar__divider"></div><button draggable="false" type="button" class="tlui-button tlui-button__normal tlui-toolbar__tools__button" data-testid="tools.arrow" data-tool="arrow" data-geo="" aria-label="tool.arrow" title="Arrow — A"><div class="tlui-icon" style="-webkit-mask: url(&quot;https://unpkg.com/@tldraw/assets@2.0.0-alpha.14/icons/icon/tool-arrow.svg&quot;) center 100% / 100% no-repeat;"></div></button><button draggable="false" type="button" class="tlui-button tlui-button__normal tlui-toolbar__tools__button" data-testid="tools.text" data-tool="text" data-geo="" aria-label="tool.text" title="Text — T"><div class="tlui-icon" style="-webkit-mask: url(&quot;https://unpkg.com/@tldraw/assets@2.0.0-alpha.14/icons/icon/tool-text.svg&quot;) center 100% / 100% no-repeat;"></div></button><button draggable="false" type="button" class="tlui-button tlui-button__normal tlui-toolbar__tools__button" data-testid="tools.note" data-tool="note" data-geo="" aria-label="tool.note" title="Note — N"><div class="tlui-icon" style="-webkit-mask: url(&quot;https://unpkg.com/@tldraw/assets@2.0.0-alpha.14/icons/icon/tool-note.svg&quot;) center 100% / 100% no-repeat;"></div></button><button draggable="false" type="button" class="tlui-button tlui-button__normal tlui-toolbar__tools__button" data-testid="tools.asset" data-tool="asset" data-geo="" aria-label="tool.asset" title="Asset — Ctrl U"><div class="tlui-icon" style="-webkit-mask: url(&quot;https://unpkg.com/@tldraw/assets@2.0.0-alpha.14/icons/icon/tool-media.svg&quot;) center 100% / 100% no-repeat;"></div></button><button draggable="false" type="button" class="tlui-button tlui-button__normal tlui-toolbar__tools__button" data-testid="tools.rectangle" data-tool="rectangle" data-geo="rectangle" aria-label="tool.rectangle" title="Rectangle — R"><div class="tlui-icon" style="-webkit-mask: url(&quot;https://unpkg.com/@tldraw/assets@2.0.0-alpha.14/icons/icon/geo-rectangle.svg&quot;) center 100% / 100% no-repeat;"></div></button><button draggable="false" type="button" class="tlui-button tlui-button__button tlui-toolbar__tools__button tlui-toolbar__overflow" data-testid="tools.more" title="More" id="radix-:r6:" aria-haspopup="menu" aria-expanded="false" data-state="closed" dir="ltr"><div class="tlui-icon" style="-webkit-mask: url(&quot;https://unpkg.com/@tldraw/assets@2.0.0-alpha.14/icons/icon/chevron-up.svg&quot;) center 100% / 100% no-repeat;"></div></button><button type="button"  class="tlui-button tlui-button__normal tlui-toolbar__tools__button" id="ext-branding"><>EXT</ div></button>';
-
-      var extButton = document.getElementById('ext-branding');
-      extButton.addEventListener('click', function () {
-        location.href = 'https://ext.store/';
-      });
-    `
-  );
+  await ext.webviews.executeJavaScript(webview.id, ExtBranding);
 };
 
 // function to create new webview
